bd90f7ae29af632844d217b6f6ca88e2a72c73f8
Arunkumar S <84271642+Arun140801@users.noreply.github.com>
2021-12-06 14:06:40 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -22,3 +22,40 @@ const useEffect = () => {
     const setCount = getCount(yourRemainder , partnerRemainder)
     const setResult = getResult(yourRemainder , partnerRemainder)
 }
+
+
+const getRemainder = (splitName, name) => {
+    console.log(splitName, name)
+
+    splitName.map(item => {
+        const regex = new RegExp(`${item}`,"i")
+        name = name.replace(regex, "")
+        console.log(name)
+    })
+    console.log(name)
+    return name
+}
+
+const getCount = (yourRemainder, partnerRemainder) => {
+    const totalWords = yourRemainder+partnerRemainder
+    const totalCount = totalWords.length
+    console.log(totalCount)
+    return totalCount
+}
+
+
+const getResult = (flames,setCount) => {
+    for (let i = 6; i > 1; i--) {
+        flames.splice((setCount-1)%i,1)
+        console.log(flames)
+        console.log((setCount-1)%i, flames.length-1)
+    
+        if ((setCount-1)%i < flames.length-1 || (setCount-1)%i != 0) {
+            flamesPart = flames.splice((setCount-1)%i,)
+            console.log(flames)
+            flames = [...flamesPart,...flames]
+            console.log(flames)
+        }
+    }
+    return flames
+}
